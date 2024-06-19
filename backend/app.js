8c21b6063506177095c344fc38a7296bcb7931ba
Faisal <faishal.55raza@gmail.com>
2024-06-19 15:44:14 +0530

    second stage

--- backend/app.js
+++ backend/app.js
@@ -35,6 +35,12 @@ app.use(fileUpload());
  app.use("/api/v1",user);
  app.use("/api/v1",payment);
  app.use("/api/v1",orders);
+
+ app.use(express.static(path.join(__dirname, "../frontend/build")));
+
+app.get("*", (req, res) => {
+  res.sendFile(path.resolve(__dirname, "../frontend/build/index.html"));
+});
  
 
  app.use(errorMiddleware);
