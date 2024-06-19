1acd2b3a739bc617ba6092ce3890272a4b63f104
Faisal <faishal.55raza@gmail.com>
2024-06-20 02:07:22 +0530

    final v-7

--- backend/utils/JWTTOKEN.js
+++ backend/utils/JWTTOKEN.js
@@ -4,14 +4,12 @@ const sendToken = (user, statusCode, res) => {
    const token = user.getJWTToken();
  
    // options for cookie
-   const options = {
-     expires: new Date(
-       Date.now() + process.env.COOKIE_EXPIRE * 24 * 60 * 60 * 1000
-     ),
-     sameSite: "none",
-     domain : "https://aio-store-fawn.vercel.app",
-     secure: true, 
-   };
+   const cookieOptions = {
+    maxAge: 15 * 24 * 60 * 60 * 1000,
+    sameSite: "none",
+    httpOnly: true,
+    secure: true,
+  };
  
    res.status(statusCode).cookie("token", token, options).json({
      success: true,
