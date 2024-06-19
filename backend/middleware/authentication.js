92cb42046a985147eca3bd75556b5153546edbfc
Faisal <faishal.55raza@gmail.com>
2024-06-19 20:23:02 +0530

    tp

--- backend/middleware/authentication.js
+++ backend/middleware/authentication.js
@@ -8,7 +8,7 @@ const User = require("../models/userModel")
 exports.isAuthentication = catchAsyncErrors( async(req,res,next) => {
   
   const { token } = req.cookies;
-
+ console.log(token)
   if (!token) {
     return next(new ErrorHander("Please Login to access this resource", 401));
   }
